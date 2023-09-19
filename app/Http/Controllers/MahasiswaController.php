54613443e620aa8368affdaf907a2e406292ac6f
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-19 21:52:01 +0700

    Merancang struktur controller mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
-    //
+    public function index()
+    {
+        return "Tabel mahasiswa ada disini";
+    }
+
+    public function create()
+    {
+        return view('form-pendaftaran');
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate(
+            [
+                'nim' => 'required|size:8',
+                'nama' => 'required|min:3|max:50',
+                'jenis_kelamin' => 'required|in:P,L',
+                'jurusan' => 'required',
+                'alamat' => '',
+            ]
+        );
+        dump($validateData);
+    }
 }
