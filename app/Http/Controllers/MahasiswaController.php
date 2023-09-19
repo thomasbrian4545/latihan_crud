ffd4f5f21f2eb7f910f72327a571c5f552977e9b
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-19 23:46:49 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -35,11 +35,14 @@ class MahasiswaController extends Controller
         return "Data berhasil diinput ke database";
     }
 
-    public function show($mahasiswa)
-    {
-        // dd($mahasiswa);
-        $result = Mahasiswa::findOrFail($mahasiswa);
-        return view('mahasiswa.show', ['mahasiswa' => $result]);
     // public function show($mahasiswa)
+    // {
+    //     // dd($mahasiswa);
+    //     $result = Mahasiswa::findOrFail($mahasiswa);
+    //     return view('mahasiswa.show', ['mahasiswa' => $result]);
+    // }
+
+    public function show(Mahasiswa $mahasiswa){
+        return view('mahasiswa.show', ['mahasiswa' => $mahasiswa]);
     }
 }
