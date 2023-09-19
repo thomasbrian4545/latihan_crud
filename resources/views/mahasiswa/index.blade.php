9e0963c9e0554b47955744dbabf454eaba7f9da5
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-20 00:04:51 +0700

    Membuat tombol Tambah Mahasiswa

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -8,8 +8,11 @@
     <div class="container mt-3">
         <div class="row">
             <div class="col-12">
-                <div class="py-4">
+                <div class="py-4 d-flex justify-content-between align-items-center">
                     <h2>Tabel Mahasiswa</h2>
+                    <a href="{{ route('mahasiswas.create') }}" class="btn btn-primary">
+                        Tambah Mahasiswa
+                    </a>
                 </div>
                 <table class="table table-striped">
                     <thead>
